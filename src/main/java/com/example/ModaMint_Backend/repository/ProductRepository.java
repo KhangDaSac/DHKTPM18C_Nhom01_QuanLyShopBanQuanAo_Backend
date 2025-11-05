5b403e63d19c5587393682795505643320b07b8a
dgkhoa279 <vodangkhoa270904@gmail.com>
2025-11-05 16:00:03 +0700

    BE: thêm endpoint xử lí lọc sản phẩm

--- src/main/java/com/example/ModaMint_Backend/repository/ProductRepository.java
+++ src/main/java/com/example/ModaMint_Backend/repository/ProductRepository.java
@@ -3,15 +3,12 @@ package com.example.ModaMint_Backend.repository;
 import com.example.ModaMint_Backend.entity.Product;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-import java.util.List;
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    
     @Override
     @EntityGraph(attributePaths = {"productVariants", "brand", "category"})
     List<Product> findAll();
